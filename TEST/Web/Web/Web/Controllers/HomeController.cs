2d489f073ed9330e5e1ca80202b22cfc569a80ac
FollowTheCreator <artyomkovalevich.top@gmail.com>
2019-03-01 17:04:19 +0300

    form ajax

--- TEST/Web/Web/Web/Controllers/HomeController.cs
+++ TEST/Web/Web/Web/Controllers/HomeController.cs
@@ -11,16 +11,42 @@ namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        //путь к бд нужно менять, '..\..\..\..\' не сработало
+        DataContext db = new DataContext(@"data source=(localdb)\MSSQLLocalDB;AttachDbFilename=C:\Users\Artyom\Test\TEST\TestDB.mdf;initial catalog=TestDB;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework");
+
         public ActionResult Index(int page = 1, string source = "%", string sort = "date")
         {
-            //путь к бд нужно менять, '..\..\..\..\' не сработало
-            DataContext db = new DataContext(@"data source=(localdb)\MSSQLLocalDB;AttachDbFilename=C:\Users\Artyom\Test\TEST\TestDB.mdf;initial catalog=TestDB;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework");
             int count = (from item in db.GetTable<rss>()
-                        where SqlMethods.Like(item.source, source)
-                        select item)
+                         where SqlMethods.Like(item.source, source)
+                         select item)
                         .Count();
             int pageSize = 10;
 
+            IEnumerable<rss> currentNews = rssQuery(sort, source, page, pageSize, db);
+
+            pageInfo pageInfo = new pageInfo { pageNumber = page, pageSize = pageSize, totalItems = count };
+            indexViewModel ivm = new indexViewModel { pageInfo = pageInfo, rssList = currentNews };
+            return View(ivm);
+        }
+
+        [HttpPost]
+        public ActionResult ajaxPageLinks(int page = 1, string source = "%", string sort = "date")
+        {
+            int count = (from item in db.GetTable<rss>()
+                         where SqlMethods.Like(item.source, source)
+                         select item)
+                        .Count();
+            int pageSize = 10;
+
+            IEnumerable<rss> currentNews = rssQuery(sort, source, page, pageSize, db);
+
+            pageInfo pageInfo = new pageInfo { pageNumber = page, pageSize = pageSize, totalItems = count };
+            indexViewModel ivm = new indexViewModel { pageInfo = pageInfo, rssList = currentNews };
+            return PartialView(ivm);
+        }
+
+        public static IEnumerable<rss> rssQuery(string sort, string source, int page, int pageSize, DataContext db)
+        {
             IEnumerable<rss> currentNews;
             if (sort == "date")
             {
@@ -40,10 +66,7 @@ namespace Web.Controllers
                                .Skip((page - 1) * pageSize)
                                .Take(pageSize);
             }
-
-            pageInfo pageInfo = new pageInfo { pageNumber = page, pageSize = pageSize, totalItems = count };
-            indexViewModel ivm = new indexViewModel { pageInfo = pageInfo, rssList = currentNews };
-            return View(ivm);
+            return currentNews;
         }
     }
 }
